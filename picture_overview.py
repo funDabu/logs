223b310a785e0ccdcf24f9253ece94e1b5eab41a
František Buš <bus@fi.muni.cz>
2022-09-30 11:02:02 +0200

    picture_overview: code refatoring

--- picture_overview.py
+++ picture_overview.py
@@ -1,6 +1,6 @@
 from log_stats import Log_stats, Stat_struct
 from PIL import Image, ImageDraw, ImageFont
-from typing import List, Optional
+from typing import Callable, List, Optional, Tuple, Iterable
 import sys
 
 
@@ -8,25 +8,27 @@ MONTHS = ["Error", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"
 
 
 def make_annotations(maximum: int) -> List[str]:
+    # Currently not used
     fifth = maximum // 5
     return [ "{:.2e}".format(fifth * i) for i in range(6) ]
 
 
-def annotate(draw: ImageDraw,
-             day_annotations: List[str],
-             month_annotations: List[str],
-             width:int, height: int,
-             zero_line: int):
+def annotate(draw: ImageDraw.ImageDraw,
+             day_maximum: int,
+             month_maximum: int,
+             width:int,
+             height: int,
+             zero_line: int,
+             font: ImageFont.ImageFont=None):
     fifth = height // 5
     y = zero_line
-
-    month_width, _ = get_text_size(month_annotations[-1])
+    label_width, _ = get_text_size("{:.2e}".format(day_maximum), font)
 
     for i in range(6):
-        draw.text((2, y), day_annotations[i], fill=(0,0,0))
-        draw.text((width - month_width - 2, y),
-                  month_annotations[i],
-                  fill=(0,0,0) )
+        draw.text((2, y), "{:.2e}".format( day_maximum // 5 * i), fill=(0,0,0))
+        draw.text((width-2-label_width, y),
+                  "{:.2e}".format( month_maximum // 5 * i),
+                  fill=(0,0,0))
         draw.rectangle( [(2, y), (width-2, y) ], fill=(100,100,100) )
         y -= fifth
 
@@ -35,13 +37,13 @@ def get_text_size(text: str, font: Optional[ImageFont.ImageFont] = None) -> int:
     if font is None:
         font = ImageFont.load_default()
     
-    # older version
-    # w, h = font.getsize(month)
+    # older version - will be removed in Pillow 10 (2023)
+    w, h = font.getsize(text)
 
-    # new version 
-    l, t, r, b = font.getbbox(text)
-    w = r-l
-    h = b-t
+    # # new version 
+    # l, t, r, b = font.getbbox(text)
+    # w = r-l
+    # h = b-t
 
     return (w, h)
 
@@ -57,6 +59,7 @@ def print_month(x: int, zero_line: int, month: str, img: Image.Image):
 
 
 def get_months_maximum(stat: Log_stats):
+    # Currently not used
 
     def get_max_month_reqs(struct: Stat_struct) -> int:
         max1 = max( struct.month_req_distrib.values() )
@@ -69,6 +72,32 @@ def get_months_maximum(stat: Log_stats):
     return max(maxs)
 
 
+def day_data_to_month_data(data:Iterable[Tuple[int, int, str]])\
+                            -> List[Tuple[int, int, str]]:
+    month_data = []
+
+    current_month = None
+    month_value = 0
+    month_width = 0
+
+    for width, value, date in data:
+        split_date = date.split("-") # date is isoformat ("YYYY-MM-DD")
+        month = f"{split_date[0]}-{split_date[1]}"
+
+        if month != current_month:
+            if current_month is not None:
+                month_data.append((month_width, month_value, current_month))
+                month_value, month_width = 0, 0
+            current_month = month
+        
+        month_width += width
+        month_value += value
+
+    month_data.append((month_width, month_value, current_month))
+    return month_data
+    
+
+
 def make_picture(stats: Optional[Log_stats] = None,
                  output_name: str = "overview.png"):
     if stats is None:
@@ -76,11 +105,16 @@ def make_picture(stats: Optional[Log_stats] = None,
     data = sorted(stats.daily_data.items(), key=lambda x: x[0])
     # print(data) #DEBUG
 
-    maximum = max(map(lambda x: x[1][1], data))
-    months_maximum = get_months_maximum(stats)
+    day_data = list(map(lambda x: (1, x[1][1], x[0].isoformat()), data))
+    # DEBUG ^make it more readabel 
+    # print(day_data) #DEBUG
+    month_data = day_data_to_month_data(day_data)
+    # print(month_data) #DEBUG
+
+
+    day_maximum = max(map(lambda x: x[1], day_data))
+    month_maximum = max(map(lambda x: x[1], month_data))
     # print(months_maximum) # DEBUG
-    day_annotations = make_annotations(maximum)
-    month_annotaions = make_annotations(months_maximum)
 
     left_margin = 60
     right_margin = 60
@@ -91,81 +125,57 @@ def make_picture(stats: Optional[Log_stats] = None,
 
     zero_line = top_margin + height
 
-
-    day_recs, month_recs, x_labels = get_rectangles(data,
-                                                    maximum,
-                                                    months_maximum,
-                                                    height,
-                                                    width,
-                                                    left_margin,
-                                                    right_margin,
-                                                    zero_line )
-    
-    # print("\n", "width:", width)
+    day_recs = get_rectangles(day_data, day_maximum, height, left_margin, zero_line)
+    month_recs = get_rectangles(month_data, month_maximum, height, left_margin, zero_line)
 
     img = Image.new('RGB', (width, height + bottom_margin + top_margin), (255,255,255))
     draw = ImageDraw.Draw(img)
 
-    for coords in month_recs:
+    for coords, _ in month_recs:
         draw.rectangle(coords, fill=(120,120,120))
-    annotate(draw, day_annotations, month_annotaions, width, height, zero_line)
-    for coords in day_recs:
+    annotate(draw, day_maximum, month_maximum, width, height, zero_line)
+    for coords,_ in day_recs:
         draw.rectangle(coords, fill=(0,0,0))
-    for x, label in x_labels:
-        print_month(x, zero_line, label, img)
+    draw_x_labels(img, month_recs, zero_line, pritify_month)
 
-
-    # month_img.paste(img)
     img.save(output_name, format="png")
 
 
-def get_rectangles(data,
-                maximum: int, # day maximum
-                months_maximum: int,
-                height: int,
-                width: int,
-                left_margin: int,
-                right_margin: int,
-                zero_line: int ):
+def get_rectangles(data: Iterable[Tuple[int, int, str]],
+                   maximum: int,
+                   height: int,
+                   x: int,
+                   zero_line: int)\
+    -> List[Tuple[ Tuple[Tuple[int, int],Tuple[int,int]], str]]:
 
-    current_month = None
-    month_value = 0
-    month_start_x = left_margin
+    rectangles = []
 
-    day_rectangles = []
-    month_rectangles = []
-    x_labels = []
+    for w, h, label in data:
+        value = round((h * height / maximum))
+        rectangles.append(([(x, zero_line), (x+w-1 , zero_line-value)], label))
+        x += w
+ 
+    return rectangles
 
-    for x, (date, (_, value)) in enumerate(data):
-        x = x*2 + left_margin
 
-        day_value = round((value * height / maximum))
-        day_rectangles.append([(x, zero_line), (x+1 , zero_line-day_value)])
-        
-        month = f"{MONTHS[date.month]} {date.year}"
-        if month != current_month:
-            # Append month rectangle and x-label
-            if current_month is not None:
-                month_value = (round(month_value * height/ months_maximum))
-                month_rectangles.append([(month_start_x, zero_line),
-                                          (x-1, zero_line-month_value)] )
-                month_start_x = x
-                month_value = 0
+def pritify_month(month: str) -> str:
+    splited_month = month.split("-")
+    pretty_month = MONTHS[int(splited_month[1])]
+    return f"{pretty_month} {splited_month[0]}"
 
-            current_month = month 
-            x_labels.append((x, month))
 
-        month_value += value
-        # print(month) # DEBUG
+def draw_x_labels(img: Image.Image,
+                  rectangles: List[Tuple[ Tuple[Tuple[int, int],
+                                                Tuple[int,int]],
+                                          str ]],
+                  zero_line: int,
+                  pritify: Callable[[str], str] = lambda x:x):
+    for coords, label in rectangles:
+        x = coords[0][0]
+        zero_line = coords[0][1]
+        print_month(x, zero_line, pritify(label), img)
 
-    # appned last month rectangle
-    month_value = (round(month_value * height / months_maximum))
-    month_rectangles.append([(month_start_x, zero_line),
-                             (width-right_margin, zero_line-month_value)])
 
-    return (day_rectangles, month_rectangles, x_labels)
-
-    
 def main():
     make_picture()
 
