cc4fae48fa9f8e1622f6b388d0ffe75c4960792a
František Buš <bus@fi.muni.cz>
2024-02-07 20:30:06 +0100

    removed spaces at the ends of ipv4 regexs

--- log_stats.py
+++ log_stats.py
@@ -22,7 +22,7 @@ from constants import LOG_DT_FORMAT, DT_FORMAT, DATE_FORMAT, MONTHS, DAYS
 BOT_URL_REGEX = r"(http\S+?)[);]"
 RE_PATTERN_BOT_URL = re.compile(BOT_URL_REGEX)
 
-SIMPLE_IPV4_REGEX = r"^\s*(?:[0-9]{1,3}\.){3}[0-9]{1,3}\s*$"
+SIMPLE_IPV4_REGEX = r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
 RE_PATTERN_SIMPLE_IPV4 = re.compile(SIMPLE_IPV4_REGEX)
 
 BOT_USER_AGENT_REGEX = r"bot|Bot|crawl|Crawl"
@@ -31,13 +31,13 @@ RE_PATTERN_BOT_USER_AGENT = re.compile(BOT_USER_AGENT_REGEX)
 # Based on https://www.fi.muni.cz/tech/unix/external-network.html.cs
 # 147.251.42–53.0/24, 147.251.58.0/24, 172.16.0.0/12
 FI_MU_IPv4_REGEX = \
-    r"^\s*147\.251\.(?:" \
+    r"^147\.251\.(?:" \
     + '|'.join(str(n) for n in range(42,54)) \
     + "|58" \
-    + r")\.[0-9]{1,3}\s*$"  \
-    + r"|^\s*172\.(?:" \
+    + r")\.[0-9]{1,3}$"  \
+    + r"|^172\.(?:" \
     + '|'.join(str(n) for n in range(16,32)) \
-    + r")(?:\.[0-9]{1,3}){2}\s*$"
+    + r")(?:\.[0-9]{1,3}){2}$"
 
 RE_PATTERN_FI_MU_IPv4 = re.compile(FI_MU_IPv4_REGEX)
 
